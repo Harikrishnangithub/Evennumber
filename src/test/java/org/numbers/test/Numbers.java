c3bded74cdb61dbbe4b3b42f55268e1f2452fa5f
Harikrishnangithub <Harikrishnangithub@gmail.com>
2020-01-07 18:05:50 +0530

    My First Commit..

--- src/test/java/org/numbers/test/Numbers.java
+++ src/test/java/org/numbers/test/Numbers.java
@@ -1,21 +1,29 @@
 package org.numbers.test;
 
-import org.openqa.selenium.By;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Numbers 
 {
 	
 	public static void main(String[] args)
-	{      
-			String month = "January";
-			String year = "2020";
-			String text = driver.findElement(By.xpath("//span[text()='December']")).getText();
-			String text1 = driver.findElement(By.xpath("//span[text()='2019']")).getText();
-			if (text.equalsIgnoreCase("month")&&text1.equalsIgnoreCase("year")) 
-			{
-				driver.findElement(By.xpath("//a[@title='Next']")).click();
+	{   // Declare two strings 
+        String a = "Hellow"; 
+        String b = "World"; 
+          
+        // Print String before swapping 
+        System.out.println("Strings before swap: a = " +  
+                                       a + " and b = "+b); 
+          
+      a = a+b;
+      b= b.substring(0,a.length()-b.length());
+      a= a.substring(b.length());
+       System.out.println("a = "+a);
+       System.out.println("b = "+b);
 			}
 			
+		
 		}
-		}
-}
+		
+
